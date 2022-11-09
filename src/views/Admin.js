f907e5acf9bfdb9b50f00e83ab0d04d4750be4e6
Lee <leerenwusegura@gmail.com>
2022-11-09 15:18:31 -0600

    Minor fixes

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Admin() {
-  return (
-    <div>Admin</div>
-  )
+    return (
+        <>
+            <div>Admin</div>
+
+            <Outlet />
+        </>
+    )
 }
 
 export default Admin
