61a387b706fe923b8378f3b0746a8e853763d4c5
Lee <leerenwusegura@gmail.com>
2022-11-30 14:20:28 -0600

    Profile page updated

--- src/views/Perfil.js
+++ src/views/Perfil.js
@@ -1,23 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
+import { Panel } from 'primereact/panel';
+import { Card } from 'primereact/card';
 
 import Header from '../components/Header'
 
 
 function Perfil() {
   const navigate = useNavigate();
-
+  const user = JSON.parse(window.sessionStorage.getItem('user'));
 
   useEffect(() => {
-    const user = JSON.parse(window.sessionStorage.getItem('user'));
     if (!user) {
       navigate(`/`);
-    } 
+    }
   }, [])
+
   return (
     <>
       <Header />
-      <div>Perfil</div>
+      <div className="panel-pages">
+        <Card title={`${user.user_Name} ${user.f_LastName} ${user.s_LastName} `} subTitle={`${user.role}`} style={{ width: '25em' }} >
+          <p>Rol: {user.role}</p>
+          <p>Correo: {user.email}</p>
+        </Card>
+      </div>
+
 
     </>
   )
